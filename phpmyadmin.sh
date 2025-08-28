ae23cd7618875ed5715552cc4b87a680e6b15bb2
Cameron Simmons <cameron.simmons.117@my.csun.edu>
2025-08-27 19:29:10 -0700

    adding start apache2

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -2,22 +2,21 @@
 echo "##################"
 echo "# Running script #"
 echo "##################"
-
 # run file as root
-sudo apt update
-sudo apt upgrade
+apt update
+apt upgrade
 # installing the mysql before the phpmyadmin
 # note for later :     mysql -u root -p
-sudo apt install mysql-server
-sudo systemctl start mysql
-sudo systemctl enable mysql
+apt install mysql-server
+systemctl start mysql
+systemctl enable mysql
 # this will install the rest of the lamp stack
-sudo apt install apache2 mysql-server php libapache2-mod-php php-mysql php-mbstring php-zip php-gd php-json php-curl
-
-sudo apt install phpmyadmin
-sudo phpenmod mbstring
-
-
+apt install apache2 mysql-server php libapache2-mod-php php-mysql php-mbstring php-zip php-gd php-json php-curl
+# starting the apache
+systemctl start apache2
+systemctl enable apache2
+apt install phpmyadmin
+phpenmod mbstring
 echo "#################"
 echo "# End of script #"
 echo "#################"
