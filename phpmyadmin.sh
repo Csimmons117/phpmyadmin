3b6aac081d292af4bd768c2d98932cfd11b89d14
Cameron Simmons <143228401+Csimmons117@users.noreply.github.com>
2025-09-13 14:57:54 -0700

    Update phpmyadmin.sh

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -10,6 +10,10 @@ apt upgrade -y
 apt install mysql-server
 systemctl start mysql
 systemctl enable mysql
+# Create an account to log in
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';"
+mysql -e "FLUSH PRIVILEGES;"
+
 # this will install the rest of the lamp stack
 apt install apache2 mysql-server php libapache2-mod-php php-mysql php-mbstring php-zip php-gd php-json php-curl
 # starting the apache
