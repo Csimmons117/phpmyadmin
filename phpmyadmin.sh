217b03a0d1242ed94b2519aeb4c20078514b15f7
Cameron <cs.simmons0921@gmail.com>
2025-09-20 13:44:49 -0700

    adding the php files

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -6,13 +6,13 @@ echo "##################"
 apt update
 apt upgrade -y
 # Install open ssh
-apt install openssh-server
+apt install openssh-server -y
 systemctl start openssh-server
 systemctl enable openssh-server
 ufw allow ssh
 # installing the mysql before the phpmyadmin
 # note for later :     mysql -u root -p
-apt install mysql-server
+apt install mysql-server -y
 systemctl start mysql
 systemctl enable mysql
 # Create an account to log in
@@ -20,12 +20,17 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY
 mysql -e "FLUSH PRIVILEGES;"
 
 # this will install the rest of the lamp stack
-apt install apache2 mysql-server php libapache2-mod-php php-mysql php-mbstring php-zip php-gd php-json php-curl
+apt install apache2 mysql-server php libapache2-mod-php php-mysql php-mbstring php-zip php-gd php-json php-curl -y
 # starting the apache
 systemctl start apache2
 systemctl enable apache2
-apt install phpmyadmin
+apt install phpmyadmin -y
 phpenmod mbstring
+wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-english.tar.gz
+tar -xzf phpMyAdmin-5.2.1-english.tar.gz
+cd phpMyAdmin-5.2.1-english/
+mv * /var/www/html/
+mv .* /var/www/html
 echo "#################"
 echo "# End of script #"
 echo "#################"
